f9f313d6d875ca886456f2661bd8462e02cb2d46
gaunthan <gaunthanhuang@foxmail.com>
2020-01-09 15:55:48 +0800

    Add script for installing docker

--- README.md
+++ README.md
@@ -8,4 +8,5 @@ Scripts for setting up a totally self-used Manjaro Linux.
 | update_cn_sources.sh        | Update system sources to China sources, boosting download speed for Chinese users. |
 | install_useful_softwares.sh | Install useful and essential softwares such as web browsers, download tools, music player and so on. |
 | setup_cv_env.sh             | Install vision and science computing toolkits such as OpenCV, anaconda, basic building tools and so on. |
+| install_docker.sh           | Install docker, enable it on system boot and make current user able to use docker commands directly. |
 
---
+++ script/install_docker.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Install docker
+sudo pacman -S docker --noconfirm
+
+# Enable docker on system booting
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Create docker group if not exists
+GROUP="docker"
+grep -q ${GROUP} /etc/group
+if [[ "$?" != "0" ]]; then
+    sudo groupadd ${GROUP}
+fi
+
+# Add current user to docker group
+sudo usermod -aG ${GROUP} ${USER}
+
+# Restart docker to make anything effect
+sudo systemctl restart docker
+
+# Check if need to log out
+id | grep -q ${GROUP}
+if [[ "$?" = "0" ]]; then
+    echo "Congrads! Now you are able to use docker commands."
+else
+    echo "Oops! You may need to log out and log back in again"\
+	 "to be able to run docker commands!"
+fi

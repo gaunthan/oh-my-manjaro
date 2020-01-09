bff0ef32c39828f795ee2fc71b665a32388e1500
gaunthan <gaunthanhuang@foxmail.com>
2020-01-09 16:26:46 +0800

    :sparles: Use docker-cn mirror if at Asia

--- script/install_docker.sh
+++ script/install_docker.sh
@@ -7,6 +7,29 @@ sudo pacman -S docker --noconfirm
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# Use docker-cn mirror if at Asia
+curl http://ip-api.com/line?fields=timezone 2> /dev/null | grep -qi Asia
+if [[ "$?" = "0" ]]; then
+    echo "Use registry mirror: docker-cn"
+
+    CONFIG_STR='{"registry-mirrors":["https://registry.docker-cn.com"]}'
+    CONFIG_FILE="/etc/docker/daemon.json"
+
+    # Create config file if not exits
+    if [[ ! -f ${CONFIG_FILE} ]]; then
+        sudo touch ${CONFIG_FILE}
+    fi
+
+    # Add mirror if not exits
+    grep -qi "docker-cn" ${CONFIG_FILE}
+    if [[ "$?" != "0" ]]; then
+	echo "Adding docker mirror to ${CONFIG_FILE}"
+        echo ${CONFIG_STR} | sudo tee -a ${CONFIG_FILE} &> /dev/null
+    fi
+
+    echo "Congrads! Mirror is added!"
+fi
+
 # Create docker group if not exists
 GROUP="docker"
 grep -q ${GROUP} /etc/group
